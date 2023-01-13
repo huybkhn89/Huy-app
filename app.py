81562e5b4105d492a31cc91fac6a64e157db0bcb
huybkhn89 <huybkhn89@gmail.com>
2023-01-14 00:37:20 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+ten = st.text_input("First Name:")
+ho = st.text_input("Last Name:")
+st.write("Hello ",ten,ho)
+
+Submit = st.button("Submit")
+if Submit:
+    st.text("Da submit")
+
+with open("E:\PY\Tep.jpg","rb") as anh:
+    st.download_button(
+        label="Tai anh",
+        data=anh,
+        file_name="Anh tai ve cua Tep.png",
+        mime="image/png"
+    )
+
+with st.form("Form đăng ký:"):
+    Name= st.text_input("Name :")
+    Phone = st.text_input("Phone :")
+    Submit = st.form_submit_button("Submit")
+    if Submit:
+        st.write(Name,"-",Phone)
